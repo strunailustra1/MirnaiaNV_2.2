877208177d371afa8dbd6e960496bb99eb9a6e03
Natalia Mirnaia <strunailustra@gmail.com>
2019-11-15 19:57:31 +0300

    Initial Commit

---
+++ MirnaiaNV_2.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MirnaiaNV_2.2
+//
+//  Created by Наталья Мирная on 15/11/2019.
+//  Copyright © 2019 Наталья Мирная. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
